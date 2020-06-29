60214b61361752d86bd5c4d731672381da92408c
Taylor McKeel <josephmckeel1993@gmail.com>
2020-06-29 18:55:14 -0400

    before demo

--- pumpkin.js
+++ pumpkin.js
@@ -1,7 +1,25 @@
-const db = require('./dbConfig');
-const Sequelize = require('sequelize');
+const db = require("./dbConfig");
+const Sequelize = require("sequelize");
+const { STRING, NUMBER, BOOLEAN, ENUM } = require("sequelize");
 
 //YOUR CODE GOES HERE
-
+const Pumpkin = db.define("Pumpkin", {
+  name: {
+    type: STRING,
+  },
+  size: {
+    type: ENUM(["small", "medium", "large"]),
+  },
+  evil: {
+    type: BOOLEAN,
+    defaultValue: false,
+  },
+  carved: {
+    type: BOOLEAN,
+  },
+  candle: {
+    type: BOOLEAN,
+  },
+});
 //--------------------
 module.exports = Pumpkin;
